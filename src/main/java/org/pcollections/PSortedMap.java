2bbe468dbd0a7e38a4f8ec5e4a2fbe549554249a
Ran Ari-Gur <22930003+ran-arigur@users.noreply.github.com>
2022-08-14 10:40:11 -0700

    Add interfaces PSortedMap and PSortedSet (#20)

---
+++ src/main/java/org/pcollections/PSortedMap.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2022 Ran Ari-Gur. All rights reserved.
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+
+package org.pcollections;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NoSuchElementException;
+
+/**
+ * <p>An immutable, persistent map from non-null keys of type K to non-null values of type V, with
+ * keys arranged in sorted order (according to some {@link java.util.Comparator}), and with various
+ * methods to obtain specific mappings or ranges of mappings based on this ordering (such as the
+ * least key greater than some instance of type K, or the sub-map with keys between two instances of
+ * type K).</p>
+ *
+ * <p>Every PSortedMap is a {@link java.util.Map} and more specifically a {@link PMap}, but as with
+ * any sorted map, a PSortedMap will only obey the general contract of those interfaces if its
+ * comparator is consistent with equals. (See {@link java.util.SortedMap} for more information.)</p>
+ *
+ * <p>Every PSortedMap is a {@link java.util.SortedMap} and more specifically a
+ * {@link java.util.NavigableMap}, but the implementations of PSortedMap provided by this library
+ * (pcollections) depart from the specification of those interfaces in a few ways:</p>
+ *
+ * <ul>
+ *   <li>headMap(...), subMap(...), and tailMap(...) are specified by SortedMap and NavigableMap to
+ *   return maps with a "restricted key range", and to throw IllegalArgumentException if this
+ *   instance already has a restricted key range and the relevant argument is outside that range.
+ *   (This ensures that map.headMap(10).headMap(15) doesn't contain elements that map.headMap(10)
+ *   does not, and that map.headMap(10).headMap(15).put(12, "x") is invalid because a mapping with
+ *   the key 12 can't be put in map.headMap(10).) This library's implementations do not throw
+ *   IllegalArgumentException, but rather, they ensure that an argument outside the applicable range
+ *   simply has no effect; so, map.headMap(10).headMap(15) is equivalent to map.headMap(10), because
+ *   map.headMap(10) already contains no elements ≥ 15. (This is also the behavior of Guava's
+ *   ImmutableSortedMap. The JDK's Collections.unmodifiableSortedMap(...) and
+ *   Collections.unmodifiableNavigableMap(...) are agnostic on this point, because they just
+ *   delegate to the underlying map.) Other implementations are encouraged to consider doing the
+ *   same, and to document their behavior in this respect. Additionally, any implementations that
+ *   <em>do</em> use the "restricted key range" concept are encouraged to document the behavior of
+ *   their minus, minusAll, plus, and plusAll methods when a key is outside the restricted key
+ *   range.</li>
+ *
+ *   <li>comparator() is specified by SortedMap to return "null if this map uses the natural
+ *   ordering of its elements". This library's implementations never return null from that method;
+ *   instead, when the map uses the natural ordering, the method returns a Comparator instance that
+ *   implements the natural ordering. (This is because this library avoids null in general; a PMap
+ *   does not allow null keys or values. This is also the behavior of Guava's ImmutableSortedMap,
+ *   which also rejects null keys and values.) Other implementations of PSortedMap are encouraged to
+ *   consider doing the same, and to document their behavior in this case (whether or not it's to
+ *   return null).</li>
+ *
+ *   <li>pollFirstEntry() and pollLastEntry() are specified by NavigableMap to mutate this map, and
+ *   are not specified to be optional operations. That's obviously not an option for a PMap, so
+ *   PSortedMap provides default implementations of these methods that simply throw
+ *   UnsupportedOperationException, which should be the right implementation for any implementation
+ *   of this interface. (This is also the behavior of the JDK's
+ *   Collections.unmodifiableNavigableMap(...) and Guava's ImmutableSortedMap.)</li>
+ * </ul>
+ *
+ * @param <K>  the type of keys maintained by this map
+ * @param <V>  the type of mapped values
+ *
+ * @author Ran Ari-Gur
+ * @since 3.2.0
+ *
+ * @see java.util.SortedMap
+ * @see java.util.NavigableMap
+ * @see java.util.Collections#unmodifiableSortedMap(java.util.SortedMap)
+ * @see java.util.Collections#unmodifiableNavigableMap(java.util.NavigableMap)
+ * @see org.pcollections.PMap
+ * @see org.pcollections.PSortedSet
+ * @see org.pcollections.TreePMap
+ */
+public interface PSortedMap<K, V> extends PMap<K, V>, NavigableMap<K, V> {
+  // methods inherited from PMap, overridden to return specifically PSortedMap:
+
+  @Override
+  public PSortedMap<K, V> plus(K key, V value);
+  @Override
+  public PSortedMap<K, V> plusAll(Map<? extends K, ? extends V> map);
+  @Override
+  public PSortedMap<K, V> minus(Object key);
+  @Override
+  public PSortedMap<K, V> minusAll(Collection<?> keys);
+
+  // methods inherited from NavigableMap, overridden to return specifically PSortedMap or
+  // PSortedSet:
+
+  @Override
+  public PSortedSet<K> descendingKeySet();
+  @Override
+  public PSortedMap<K, V> descendingMap();
+  @Override
+  public PSortedMap<K, V> headMap(K toKey);
+  @Override
+  public PSortedMap<K, V> headMap(K toKey, boolean inclusive);
+  @Override
+  public PSortedSet<K> keySet();
+  @Override
+  public PSortedSet<K> navigableKeySet();
+  @Override
+  public PSortedMap<K, V> subMap(K fromKey, K toKey);
+  @Override
+  public PSortedMap<K, V> subMap(K fromKey, boolean fromInclusive, K toKey, boolean toInclusive);
+  @Override
+  public PSortedMap<K, V> tailMap(K fromKey);
+  @Override
+  public PSortedMap<K, V> tailMap(K fromKey, boolean inclusive);
+
+  // other methods:
+
+  /** @return The comparator used to order the keys in this map. (Never null.) */
+  @Override
+  public Comparator<? super K> comparator();
+
+  /**
+   * @return This map, minus its first mapping (the mapping with the least/lowest key).
+   * @throws NoSuchElementException - if this map is empty
+   */
+  public PSortedMap<K, V> minusFirstEntry();
+
+  /**
+   * @return This map, minus its last mapping (the mapping with the greatest/highest key).
+   * @throws NoSuchElementException - if this map is empty
+   */
+  public PSortedMap<K, V> minusLastEntry();
+
+  /**
+   * @throws UnsupportedOperationException - always
+   * @deprecated Unsupported operation.
+   */
+  @Deprecated
+  @Override
+  default Map.Entry<K, V> pollFirstEntry() {
+    throw new UnsupportedOperationException("This map instance is unmodifiable");
+  }
+
+  /**
+   * @throws UnsupportedOperationException - always
+   * @deprecated Unsupported operation.
+   */
+  @Deprecated
+  @Override
+  default Map.Entry<K, V> pollLastEntry() {
+    throw new UnsupportedOperationException("This map instance is unmodifiable");
+  }
+}
---
+++ src/main/java/org/pcollections/PSortedSet.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2022 Ran Ari-Gur. All rights reserved.
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
+
+package org.pcollections;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.NoSuchElementException;
+
+/**
+ * <p>An immutable, persistent set of distinct, non-null elements, with elements arranged in sorted
+ * order (according to some {@link java.util.Comparator}), and with various methods to obtain
+ * specific elements or ranges of elements based on this ordering (such as the least element greater
+ * than some value, or the set of elements between two values).</p>
+ *
+ * <p>(Note: this is different from {@link POrderedSet}, which keeps elements in the order that
+ * they were added to the set.)</p>
+ *
+ * <p>Every PSortedSet is a {@link java.util.Set} and more specifically a {@link PSet}, but as with
+ * any sorted set, a PSortedSet will only obey the general contract of those interfaces if its
+ * comparator is consistent with equals. (See {@link java.util.SortedSet} for more information.)</p>
+ *
+ * <p>Every PSortedSet is a {@link java.util.SortedSet} and more specifically a
+ * {@link java.util.NavigableSet}, but the implementations of PSortedSet provided by this library
+ * (pcollections) depart from the specification of those interfaces in a few ways:</p>
+ *
+ * <ul>
+ *   <li>headSet(...), subSet(...), and tailSet(...) are specified by SortedSet and NavigableSet to
+ *   return sets with a "restricted range", and to throw IllegalArgumentException if this instance
+ *   already has a restricted range and the relevant argument is outside that range. (This ensures
+ *   that set.headSet(10).headSet(15) doesn't contain elements that set.headSet(10) does not, and
+ *   that set.headSet(10).headSet(15).add(12) is invalid because 12 can't be added to
+ *   set.headSet(10).) This library's implementations do not throw IllegalArgumentException, but
+ *   rather, they ensure that an argument outside the applicable range simply has no effect; so,
+ *   set.headSet(10).headSet(15) is equivalent to set.headSet(10), because set.headSet(10) already
+ *   contains no elements ≥ 15. (This is also the behavior of Guava's ImmutableSortedSet. The JDK's
+ *   Collections.unmodifiableSortedSet(...) and Collections.unmodifiableNavigableSet(...) are
+ *   agnostic on this point, because they just delegate to the underlying set.) Other
+ *   implementations are encouraged to consider doing the same, and to document their behavior in
+ *   this respect. Additionally, any implementations that <em>do</em> use the "restricted range"
+ *   concept are encouraged to document the behavior of their minus, minusAll, plus, and plusAll
+ *   methods when a value is outside the restricted range.</li>
+ *
+ *   <li>comparator() is specified by SortedSet to return "null if this set uses the natural
+ *   ordering of its elements". This library's implementations never return null from that method;
+ *   instead, when the set uses the natural ordering, the method returns a Comparator instance that
+ *   implements the natural ordering. (This is because this library avoids null in general; a PSet
+ *   does not allow null elements. This is also the behavior of Guava's ImmutableSortedSet, which
+ *   also rejects null elements.) Other implementations of PSortedSet are encouraged to consider
+ *   doing the same, and to document their behavior in this case (whether or not it's to return
+ *   null).</li>
+ *
+ *   <li>pollFirst() and pollLast() are specified by NavigableSet to mutate this set, and
+ *   are not specified to be optional operations. That's obviously not an option for a PSet, so
+ *   PSortedSet provides default implementations of these methods that simply throw
+ *   UnsupportedOperationException, which should be the right implementation for any implementation
+ *   of this interface. (This is also the behavior of the JDK's
+ *   Collections.unmodifiableNavigableSet(...) and Guava's ImmutableSortedSet.)</li>
+ * </ul>
+ *
+ * @param <E>  the type of elements maintained by this set
+ *
+ * @author Ran Ari-Gur
+ * @since 3.2.0
+ *
+ * @see java.util.SortedSet
+ * @see java.util.NavigableSet
+ * @see java.util.Collections#unmodifiableSortedSet(java.util.SortedSet)
+ * @see java.util.Collections#unmodifiableNavigableSet(java.util.NavigableSet)
+ * @see org.pcollections.PSet
+ * @see org.pcollections.PSortedMap
+ * @see org.pcollections.TreePSet
+ */
+public interface PSortedSet<E> extends PSet<E>, NavigableSet<E> {
+  /**
+   * @return The comparator used to order the elements in this set. May be null if this set uses the
+   *         natural ordering of its elements, though in that case the implementations provided by
+   *         this library (pcollections) return a Comparator instance that implements the natural
+   *         ordering.
+   */
+  @Override
+  public Comparator<? super E> comparator();
+
+  @Override
+  public PSortedSet<E> descendingSet();
+
+  @Override
+  public PSortedSet<E> headSet(E toElement);
+
+  @Override
+  public PSortedSet<E> headSet(E toElement, boolean inclusive);
+
+  /**
+   * @param e
+   * @return This set, except with e removed (if e is an element of this set).
+   * @throws NullPointerException if e is null
+   * @throws ClassCastException if the type of e is incompatible with this set (optional)
+   * @throws IllegalArgumentException if e is incompatible with this set (optional)
+   */
+  @Override
+  public PSortedSet<E> minus(Object e);
+
+  /**
+   * @param list
+   * @return This set, except with the elements of list removed (if they are elements of this set).
+   * @throws NullPointerException if list is null or contains null
+   * @throws ClassCastException if list contains an element whose type is incompatible with this
+   *                            set (optional)
+   * @throws IllegalArgumentException if list contains an element that is incompatible with this
+   *                                  set (optional)
+   */
+  @Override
+  public PSortedSet<E> minusAll(Collection<?> list);
+
+  /**
+   * @return This set, except with its first (least) element removed.
+   * @throws NoSuchElementException if this set is empty
+   */
+  public PSortedSet<E> minusFirst();
+
+  /**
+   * @return This set, except with its last (greatest) element removed.
+   * @throws NoSuchElementException if this set is empty
+   */
+  public PSortedSet<E> minusLast();
+
+  /**
+   * @param e
+   * @return This set, except with e added (unless e is already an element of this set).
+   * @throws NullPointerException if e is null
+   * @throws IllegalArgumentException if e is incompatible with this set (optional)
+   */
+  @Override
+  public PSortedSet<E> plus(E e);
+
+  /**
+   * @param list
+   * @return This set, except with the elements of list added (unless they are already elements of
+   *         this set).
+   * @throws NullPointerException if list is null or contains null
+   * @throws IllegalArgumentException if list contains an element that is incompatible with this
+   *                                  set (optional)
+   */
+  @Override
+  public PSortedSet<E> plusAll(Collection<? extends E> list);
+
+  @Override
+  public PSortedSet<E> subSet(E fromElement, E toElement);
+
+  @Override
+  public PSortedSet<E> subSet(
+      E fromElement, boolean fromInclusive, E toElement, boolean toInclusive);
+
+  @Override
+  public PSortedSet<E> tailSet(E fromElement);
+
+  @Override
+  public PSortedSet<E> tailSet(E fromElement, boolean inclusive);
+
+  /**
+   * @throws UnsupportedOperationException always
+   * @deprecated Unsupported operation.
+   */
+  @Deprecated
+  @Override
+  default E pollFirst() {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * @throws UnsupportedOperationException always
+   * @deprecated Unsupported operation.
+   */
+  @Deprecated
+  @Override
+  default E pollLast() {
+    throw new UnsupportedOperationException();
+  }
+}
