bc39230f1dc4c3440d60e4b8b724ad6c89d8bc36
Harold Cooper <harold@x.st>
2022-09-11 16:41:39 -0400

    missing final keywords

--- src/main/java/org/pcollections/OrderedPSet.java
+++ src/main/java/org/pcollections/OrderedPSet.java
@@ -33,39 +33,39 @@ public class OrderedPSet<E> extends AbstractSet<E> implements POrderedSet<E>, Se
     return OrderedPSet.<E>empty().plus(e);
   }
 
-  private PSet<E> contents;
-  private PVector<E> order;
+  private final PSet<E> contents;
+  private final PVector<E> order;
 
-  private OrderedPSet(PSet<E> c, PVector<E> o) {
+  private OrderedPSet(final PSet<E> c, final PVector<E> o) {
     contents = c;
     order = o;
   }
 
   @Override
-  public OrderedPSet<E> plus(E e) {
+  public OrderedPSet<E> plus(final E e) {
     if (contents.contains(e)) return this;
     return new OrderedPSet<E>(contents.plus(e), order.plus(e));
   }
 
   @Override
-  public OrderedPSet<E> plusAll(Collection<? extends E> list) {
+  public OrderedPSet<E> plusAll(final Collection<? extends E> list) {
     OrderedPSet<E> s = this;
-    for (E e : list) {
+    for (final E e : list) {
       s = s.plus(e);
     }
     return s;
   }
 
   @Override
-  public OrderedPSet<E> minus(Object e) {
+  public OrderedPSet<E> minus(final Object e) {
     if (!contents.contains(e)) return this;
     return new OrderedPSet<E>(contents.minus(e), order.minus(e));
   }
 
   @Override
-  public OrderedPSet<E> minusAll(Collection<?> list) {
+  public OrderedPSet<E> minusAll(final Collection<?> list) {
     OrderedPSet<E> s = this;
-    for (Object e : list) {
+    for (final Object e : list) {
       s = s.minus(e);
     }
     return s;
@@ -82,12 +82,12 @@ public class OrderedPSet<E> extends AbstractSet<E> implements POrderedSet<E>, Se
   }
 
   @Override
-  public E get(int index) {
+  public E get(final int index) {
     return order.get(index);
   }
 
   @Override
-  public int indexOf(Object o) {
+  public int indexOf(final Object o) {
     if (!contents.contains(o)) return -1;
     return order.indexOf(o);
   }
